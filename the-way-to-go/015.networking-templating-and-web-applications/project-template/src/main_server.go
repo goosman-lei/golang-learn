1ab194a334d1d2129f55f7b182b2f13b15ea4f44
goosman.lei <goosman.lei@gmail.com>
2017-07-28 16:51:13 +0800

    commit project template

---
+++ the-way-to-go/015.networking-templating-and-web-applications/project-template/src/main_server.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+    "fmt"
+)
+
+func main() {
+    fmt.Printf("Server: Hello world!\n")
+}
---
+++ the-way-to-go/015.networking-templating-and-web-applications/project-template/src/pkg/pkg_test.go
@@ -0,0 +1,9 @@
+package pkg
+
+import (
+    "testing"
+)
+
+func TestPkg(t *testing.T) {
+    t.Log("Pass")
+}
